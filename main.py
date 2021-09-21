88bd98efb5e401a0cb08854b670e4fd5b2262836
Fiskless <Fiskless@gmail.com>
2021-09-21 17:08:26 +0500

    Add async function

--- main.py
+++ main.py
@@ -1,26 +1,64 @@
 import time
+import asyncio
 import curses
 
 
 def draw(canvas):
-    row, column = (5, 20)
     curses.curs_set(False)
     canvas.border()
+    coroutine_1 = blink(canvas, 5, 5)
+    coroutine_2 = blink(canvas, 5, 10)
+    coroutine_3 = blink(canvas, 5, 15)
+    coroutine_4 = blink(canvas, 5, 20)
+    coroutine_5 = blink(canvas, 5, 25)
+    coroutines = [
+        coroutine_1,
+        coroutine_2,
+        coroutine_3,
+        coroutine_4,
+        coroutine_5
+    ]
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
+        for coroutine in coroutines.copy():
+            coroutine.send(None)
+            canvas.refresh()
         time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
+        for coroutine in coroutines.copy():
+            coroutine.send(None)
+            canvas.refresh()
         time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
+        for coroutine in coroutines.copy():
+            coroutine.send(None)
+            canvas.refresh()
         time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
+        for coroutine in coroutines.copy():
+            coroutine.send(None)
+            canvas.refresh()
         time.sleep(0.3)
+        canvas.refresh()
+
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 if __name__ == '__main__':
+
+    TIC_TIMEOUT = 0.1
+
     curses.update_lines_cols()
     curses.wrapper(draw)
+
